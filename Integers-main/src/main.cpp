b95a59becad8b760a3760e5887a6cf0163fe6283
Ben-forde <101066891+Ben-forde@users.noreply.github.com>
2023-03-30 14:15:58 +1300

    Add files via upload

---
+++ Integers-main/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Integer.h"
+#include "Rational.h"
+
+
+using namespace cosc326;
+using namespace std;
+
+int main() {
+
+    Rational t = Rational("-3.1/2");
+
+    cout << t;
+    return 0;
+};
+
